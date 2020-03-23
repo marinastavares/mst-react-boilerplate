933fd8b6c5ffc87c5d5121545c00852957c25f72
Marina Tavares <marinastavares6@gmail.com>
2020-03-23 13:58:03 -0300

    try to add react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './App'
 
-render(<App />, document.getElementById('react-app-container'))
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state;
+  }
+}
+
+const store = createStore(countReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('react-app-container'),
+)

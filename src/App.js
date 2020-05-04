c6aca3db2f38a3d7e7e494c34c79196994f97bac
Marina Tavares <marinastavares6@gmail.com>
2020-05-04 19:35:11 -0300

    feat: change text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const App = () => <h1>Hallo</h1>
+const App = () => <h1>04/05 o dia que mst venceu a guerra pra buildar um projeto com webpack</h1>
 
 export default App
